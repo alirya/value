7779f48c094877325dc2a58a993d8db99c94aa5b
dikac <rigel234@yahoo.com>
2020-07-03 17:45:08 +0700

    move property value to "t-object"

--- dist/object-property.d.ts
+++
@@ -1,8 +0,0 @@
-import Value from "./value";
-export default class ObjectProperty<Key extends keyof Object, Object extends object> implements Value<Object[Key]> {
-    object: Object;
-    key: Key;
-    constructor(object: Object, key: Key);
-    get value(): Object[Key];
-    set value(value: Object[Key]);
-}
