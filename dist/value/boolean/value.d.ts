ee29543f8dc7f2cffd3190f587687de5864b9885
dikac <rigel234@yahoo.com>
2020-10-21 16:11:23 +0700

    fix commit missing file

--- dist/value/boolean/value.d.ts
+++
@@ -1,2 +0,0 @@
-import ValueInterface from "../../value";
-export default function Value<ValueType>(value: ValueInterface<unknown>, guard: (value: unknown) => value is ValueType): value is ValueInterface<ValueType>;
--- dist/value/boolean/value.js
+++
@@ -1,5 +0,0 @@
-import ValueObject from "../../boolean/value";
-export default function Value(value, guard) {
-    return ValueObject(value) && guard(value.value);
-}
-//# sourceMappingURL=value.js.map
